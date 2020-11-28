323b0d67b60d09ea2ba12214616c21b8aa978eaf
jgambard <jmichel.gambard@gmail.com>
2020-11-28 16:32:37 +0100

    strlcat

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ MANDATORY		= memset bzero memcpy memccpy memmove memchr memcmp strlen isalpha is
 BONUS			= lstnew lstadd_front lstsize lstlast lstadd_back lstdelone lstclear lstiter lstmap
 
 CC		= clang++
-CFLAGS	= -g3 -std=c++98 -I utils/ -I..
+CFLAGS	= -g3 -std=c++98 -I utils/ -I.. -lbsd
 
 $(MANDATORY): %: mandatory_start
 	@$(CC) $(CFLAGS) $(UTILS) $(TESTS_PATH)ft_$@_test.cpp -L.. -lft && ./a.out && rm -f a.out
---
+++ tests/ft_strlcat_test.cpp
@@ -0,0 +1,30 @@
+extern "C"
+{
+#define new tripouille
+#include "libft.h"
+#undef new
+}
+
+#include "sigsegv.hpp"
+#include <string.h>
+
+void check(bool succes) {if (succes) cout << FG_GREEN << "OK "; else cout << FG_RED << "KO ";}
+
+int main(void)
+{
+	signal(SIGSEGV, sigsegv);
+	cout << FG_LGRAY << "ft_strlcat\t: ";
+	char dest[30]; memset(dest, 0, 30);
+	char src[10]; memset(src, 'A', 9); src[9] = 0;
+	dest[0] = 'B';
+	check(ft_strlcat(dest, src, 0) == strlen(src) && dest[0] == 'B');
+	dest[0] = 'B';
+	check(ft_strlcat(dest, src, 1) == 10 && dest[0] == 'B' && dest[1] == 0);
+	memset(dest, 'B', 4);
+	check(ft_strlcat(dest, src, 3) == 3 + strlen(src) && dest[3] == 'B' && dest[4] == 0);
+	check(ft_strlcat(dest, src, 6) == 13 && !memcmp(dest, "BBBBA", 6));
+	memset(dest, 'C', 5);
+	check(ft_strlcat(dest, src, -1) == 14 && !memcmp(dest, "CCCCCAAAAAAAAA", 15));
+	cout << ENDL;
+	return (0);
+}
