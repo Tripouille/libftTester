75c9a5f3c6e9109db636024cef596b2ba256289b
jgambard <jmichel.gambard@gmail.com>
2020-11-28 17:00:01 +0100

    add atoi

--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@ UTILS			= utils/sigsegv.cpp utils/color.cpp
 TESTS_PATH		= tests/
 MANDATORY		= memset bzero memcpy memccpy memmove memchr memcmp strlen isalpha isdigit isalnum \
 				isascii isprint toupper tolower strchr strrchr strncmp strlcpy strlcat strnstr \
-				atoi calloc strdup substr strjoin strtrim split itoa strmapi putchar_fd putstr_fd \
+				atoi #calloc strdup substr strjoin strtrim split itoa strmapi putchar_fd putstr_fd \
 				putendl_fd putnbr_fd
 BONUS			= lstnew lstadd_front lstsize lstlast lstadd_back lstdelone lstclear lstiter lstmap
 
 CC		= clang++
-CFLAGS	= -g3 -std=c++98 -I utils/ -I.. -lbsd
+CFLAGS	= -g3 -std=c++11 -I utils/ -I.. -lbsd
 
 $(MANDATORY): %: mandatory_start
 	@$(CC) $(CFLAGS) $(UTILS) $(TESTS_PATH)ft_$@_test.cpp -L.. -lft && ./a.out && rm -f a.out
---
+++ tests/ft_atoi_test.cpp
@@ -0,0 +1,34 @@
+extern "C"
+{
+#define new tripouille
+#include "libft.h"
+#undef new
+}
+
+#include "sigsegv.hpp"
+#include <string.h>
+#include <climits>
+
+void check(bool succes) {if (succes) cout << FG_GREEN << "OK "; else cout << FG_RED << "KO ";}
+
+int main(void)
+{
+	signal(SIGSEGV, sigsegv);
+	cout << FG_LGRAY << "ft_atoi\t\t: ";
+
+	char escape[] = {9, 10, 11, 12, 13, 0};
+	string e(escape);
+	check(ft_atoi((e + "1").c_str()) == 1);
+	check(ft_atoi((e + "a1").c_str()) == 0);
+	check(ft_atoi((e + "--1").c_str()) == 0);
+	check(ft_atoi((e + "++1").c_str()) == 0);
+	check(ft_atoi((e + "+1").c_str()) == 1);
+	check(ft_atoi((e + "-1").c_str()) == -1);
+	check(ft_atoi((e + "0").c_str()) == 0);
+	check(ft_atoi((e + "+42lyon").c_str()) == 42);
+	check(ft_atoi((e + "+101").c_str()) == 101);
+	check(ft_atoi((e + to_string(INT_MAX)).c_str()) == INT_MAX);
+	check(ft_atoi((e + to_string(INT_MIN)).c_str()) == INT_MIN);
+	cout << ENDL;
+	return (0);
+}
