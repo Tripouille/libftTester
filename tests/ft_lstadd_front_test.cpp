1fe169c179051a13b32075a55201cb19bc403977
jgambard <jmichel.gambard@gmail.com>
2020-11-29 02:14:14 +0100

    add listadd_front

---
+++ tests/ft_lstadd_front_test.cpp
@@ -0,0 +1,33 @@
+extern "C"
+{
+#define new tripouille
+#include "libft.h"
+#undef new
+}
+
+#include "sigsegv.hpp"
+#include <string.h>
+#include <malloc.h>
+
+void check(bool succes) {if (succes) cout << FG_GREEN << "OK "; else cout << FG_RED << "KO ";}
+
+void freeList(t_list *head) {if (head) freeList(head->next); free(head);}
+
+int main(void)
+{
+	signal(SIGSEGV, sigsegv);
+	cout << FG_LGRAY << "ft_lstadd_front\t: ";
+
+	t_list * l =  NULL;
+	ft_lstadd_front(&l, ft_lstnew((void*)1));
+	check(l->content == (void*)1);
+	check(l->next == 0);
+
+	ft_lstadd_front(&l, ft_lstnew((void*)2));
+	check(l->content == (void*)2);
+	check(l->next->content == (void*)1);
+	check(l->next->next == 0);
+	freeList(l);
+	cout << ENDL;
+	return (0);
+}
