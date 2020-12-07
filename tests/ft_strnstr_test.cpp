4390a2a56e1839a54c3d10b4fbfb367a983d382d
Tripouille <jgambard@student.42lyon.fr>
2020-12-07 14:41:08 +0100

    add some tests in strnstr

--- tests/ft_strnstr_test.cpp
+++ tests/ft_strnstr_test.cpp
@@ -15,18 +15,20 @@ int main(void)
 	signal(SIGSEGV, sigsegv);
 	cout << FG_LGRAY << "ft_strnstr\t: ";
 
-	char haystack[30] = "aaabc";
+	char haystack[30] = "aaabcabcd";
 	char needle[10] = "aabc";
-	char empty[] = "";
+	char * empty = (char *)malloc(1); empty[0] = 0;
 	/* 1 */ check(ft_strnstr(haystack, needle, 0) == 0);
 	/* 2 */ check(ft_strnstr(haystack, needle, -1) == haystack + 1);
 	/* 3 */ check(ft_strnstr(haystack, "a", -1) == haystack);
-	/* 4 */ check(ft_strnstr(haystack, "c", -1) == haystack + strlen(haystack) - 1);
+	/* 4 */ check(ft_strnstr(haystack, "c", -1) == haystack + 4);
 	/* 5 */ check(ft_strnstr(empty, "", -1) == empty);
 	/* 6 */ check(ft_strnstr(empty, "", 0) == empty);
 	/* 7 */ check(ft_strnstr(empty, "coucou", -1) == 0);
 	/* 8 */ check(ft_strnstr(haystack, "aaabc", 5) == haystack);
-	static char * s = (char *)malloc(1); (void)s;
+	/* 9 */ check(ft_strnstr(empty, "12345", 5) == 0); free(empty);
+	/* 10 */ check(ft_strnstr(haystack, "abcd", 9) == haystack + 5);
+	/* 11 */ check(ft_strnstr(haystack, "cd", 8) == NULL);
 	cout << ENDL;
 	return (0);
 }
