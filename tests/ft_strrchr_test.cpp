baf09d8b75dc656244b45c5cca62955338e3675f
Tripouille <jmichel.gambard@gmail.com>
2021-04-15 13:25:46 -0400

    add one test in strrchr #aperez-b

--- tests/ft_strrchr_test.cpp
+++ tests/ft_strrchr_test.cpp
@@ -20,9 +20,11 @@ int main(void)
 	/* 1 */ check(ft_strrchr(s, 't') == s); showLeaks();
 	/* 2 */ check(ft_strrchr(s, 'l') == s + 8); showLeaks();
 	/* 3 */ check(ft_strrchr(s2, 'l') == s2 + 9); showLeaks();
-	/* 4 */ check(ft_strrchr(s, 'z') == 0); showLeaks();
+	/* 4 */ check(ft_strrchr(s, 'z') == NULL); showLeaks();
 	/* 5 */ check(ft_strrchr(s, 0) == s + strlen(s)); showLeaks();
 	/* 6 */ check(ft_strrchr(s, 't' + 256) == s); showLeaks();
+	char * empty = (char*)calloc(1, 1);
+	/* 7 aperez-b */ check(ft_strrchr(empty, 'V') == NULL); free(empty); showLeaks();
 	write(1, "\n", 1);
 	return (0);
 }
