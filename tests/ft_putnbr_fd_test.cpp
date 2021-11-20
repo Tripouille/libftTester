fed5efec440bfbd6a4b9735d8cb0f596e540b3bc
wleite <wleite@student.42sp.org.br>
2021-11-20 18:13:31 -0300

    fix: fixed ft_putnbr_fd test file permission on some systems

--- tests/ft_putnbr_fd_test.cpp
+++ tests/ft_putnbr_fd_test.cpp
@@ -20,35 +20,35 @@ int main(void)
 	signal(SIGSEGV, sigsegv);
 	title("ft_putnbr_fd\t: ")
 
-	int fd = open("tripouille", O_RDWR | O_CREAT);
+	int fd = open("tripouille", O_RDWR | O_CREAT, 0777);
 	ft_putnbr_fd(0, fd);
 	lseek(fd, SEEK_SET, 0);
 	char s[42] = {0}; read(fd, s, 2);
 	/* 1 */ check(!strcmp(s, "0")); showLeaks();
 	unlink("./tripouille");
 
-	fd = open("tripouille", O_RDWR | O_CREAT);
+	fd = open("tripouille", O_RDWR | O_CREAT, 0777);
 	ft_putnbr_fd(10, fd);
 	lseek(fd, SEEK_SET, 0);
 	read(fd, s, 3);
 	/* 2 */ check(!strcmp(s, "10")); showLeaks();
 	unlink("./tripouille");
 
-	fd = open("tripouille", O_RDWR | O_CREAT);
+	fd = open("tripouille", O_RDWR | O_CREAT, 0777);
 	ft_putnbr_fd(INT_MAX, fd);
 	lseek(fd, SEEK_SET, 0);
 	read(fd, s, 11);
 	/* 3 */ check(!strcmp(s, to_string(INT_MAX).c_str())); showLeaks();
 	unlink("./tripouille");
 
-	fd = open("tripouille", O_RDWR | O_CREAT);
+	fd = open("tripouille", O_RDWR | O_CREAT, 0777);
 	ft_putnbr_fd(INT_MIN, fd);
 	lseek(fd, SEEK_SET, 0);
 	read(fd, s, 12);
 	/* 4 */ check(!strcmp(s, to_string(INT_MIN).c_str())); showLeaks();
 	unlink("./tripouille");
 
-	fd = open("tripouille", O_RDWR | O_CREAT);
+	fd = open("tripouille", O_RDWR | O_CREAT, 0777);
 	ft_putnbr_fd(-42, fd);
 	lseek(fd, SEEK_SET, 0);
 	s[read(fd, s, 4)] = 0;
