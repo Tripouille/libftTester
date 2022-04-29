f5ffb18a49d07c65206700a10ee7a1a375b7bbae
wleite <wleite@student.42sp.org.br>
2022-04-30 01:17:38 +0200

    fix: fixed memory leak in calloc test when SIZE_MAX can be allocated

--- tests/ft_calloc_test.cpp
+++ tests/ft_calloc_test.cpp
@@ -17,10 +17,11 @@ int main(void)
 	title("ft_calloc\t: ")
 
 	void * p = ft_calloc(2, 2);
+	void * p2 = ft_calloc(SIZE_MAX, SIZE_MAX);
 	char e[] = {0, 0, 0, 0};
 	/* 1 */ check(!memcmp(p, e, 4));
-	/* 2 */ mcheck(p, 4); free(p); showLeaks();
-	/* 3 */ check(ft_calloc(SIZE_MAX, SIZE_MAX) == NULL);
+	/* 2 */ mcheck(p, 4); free(p);
+	/* 3 */ check(p2 == NULL); free(p2); showLeaks();
 	write(1, "\n", 1);
 	return (0);
 }
