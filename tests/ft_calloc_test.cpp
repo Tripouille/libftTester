9049081ad4e4ed43610a5aa35110afc470fa3cc0
Anderson Porto <andersonhigo2@gmail.com>
2022-04-29 21:23:57 -0300

    build: errno test on ft_calloc

--- tests/ft_calloc_test.cpp
+++ tests/ft_calloc_test.cpp
@@ -9,6 +9,7 @@ extern "C"
 #include "check.hpp"
 #include "leaks.hpp"
 #include <string.h>
+#include <errno.h>
 
 int iTest = 1;
 int main(void)
@@ -21,6 +22,7 @@ int main(void)
 	/* 1 */ check(!memcmp(p, e, 4));
 	/* 2 */ mcheck(p, 4); free(p); showLeaks();
 	/* 3 */ check(ft_calloc(SIZE_MAX, SIZE_MAX) == NULL);
+	/* 4 */ check(errno == ENOMEM);
 	write(1, "\n", 1);
 	return (0);
 }
