b43d17503dd75629062f3327e5b6fc1aeaec280e
mirsella <mirsella@protonmail.com>
2022-11-21 14:35:15 +0100

    add test for ft_memcpy as of november 2022 the moulinette in Paris test for ft_memcpy(dest, NULL, 0); and expect ft_memcpy to copy nothing and return a pointer to dest, as said in the man.

--- tests/ft_memcpy_test.cpp
+++ tests/ft_memcpy_test.cpp
@@ -20,12 +20,14 @@ int main(void)
 	memset(dest, 'A', 100);
 	ft_memcpy(dest, "coucou", 0);
 	/* 1 */ check(dest[0] == 'A'); showLeaks();
+	char *rtn = (char *)ft_memcpy(dest, NULL, 0);
+	/* 2 */ check(rtn == dest && dest[0] == 'A'); showLeaks();
 	char src[] = {0, 0};
 	ft_memcpy(dest, src, 2);
 	int i = 0;
 	for (; i < 100 && dest[i] == 0; ++i)
 		;
-	/* 2 */ check(i == 2 && dest[2] == 'A'); showLeaks();
+	/* 3 */ check(i == 2 && dest[2] == 'A'); showLeaks();
 	write(1, "\n", 1);
 	return (0);
 }
