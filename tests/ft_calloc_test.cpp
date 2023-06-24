6576497cbc95f35f5b7ed2cfa6be0172a7da4d5d
Everton P <evertonportela@outlook.com>
2023-06-24 13:15:02 -0300

    update - ft_calloc, test should null when insert zero (#40)
    * update - ft_calloc, test should null when insert zero
    
    * update tests calloc
    
    * Update tests/ft_calloc_test.cpp
    
    Co-authored-by: Jean-Michel <jmichel.gambard@gmail.com>
    
    * Update tests/ft_calloc_test.cpp
    
    Co-authored-by: Jean-Michel <jmichel.gambard@gmail.com>
    
    * Update ft_calloc_test.cpp
    
    Added comments about additional tests for the calloc function
    
    * Update ft_calloc_test.cpp
    
    show leaks for tests 5, 12 and 13
    
    ---------
    
    Co-authored-by: Jean-Michel <jmichel.gambard@gmail.com>

--- tests/ft_calloc_test.cpp
+++ tests/ft_calloc_test.cpp
@@ -9,6 +9,7 @@ extern "C"
 #include "check.hpp"
 #include "leaks.hpp"
 #include <string.h>
+#include <limits.h>
 
 int iTest = 1;
 int main(void)
@@ -20,7 +21,28 @@ int main(void)
 	char e[] = {0, 0, 0, 0};
 	/* 1 */ check(!memcmp(p, e, 4));
 	/* 2 */ mcheck(p, 4); free(p); showLeaks();
-	/* 3 */ check(ft_calloc(SIZE_MAX, SIZE_MAX) == NULL);
+	/* 3 */ check(ft_calloc(SIZE_MAX, SIZE_MAX) == NULL); showLeaks()
+
+	/* @evportel */
+	/* The following tests are not supported by the function's documentation. 
+ 	* But some effects returned in the trait by Moulinette so the following 
+  	* tests were implemented. */
+	/* 4 */ check(ft_calloc(INT_MAX, INT_MAX) == NULL);
+	/* 5 */ check(ft_calloc(INT_MIN, INT_MIN) == NULL); showLeaks();
+	p = ft_calloc(0, 0);
+	/* 6 */ check(p != NULL); free(p); //showLeaks();
+	p = ft_calloc(0, 5);
+	/* 7 */ check(p != NULL); free(p); //showLeaks();
+	p = ft_calloc(5, 0);
+	/* 8 */ check(p != NULL); free(p); //showLeaks();
+	/* 9 */ check(ft_calloc(-5, -5) == NULL); showLeaks();
+	p = ft_calloc(0, -5);
+	/* 10 */ check(p != NULL); free(p); //showLeaks();
+	p = ft_calloc(-5, 0);
+	/* 11 */ check(p != NULL); free(p); //showLeaks();
+	/* 12 */ check(ft_calloc(3, -5) == NULL); showLeaks();
+	/* 13 */ check(ft_calloc(-5, 3) == NULL); showLeaks();
+
 	write(1, "\n", 1);
 	return (0);
 }
